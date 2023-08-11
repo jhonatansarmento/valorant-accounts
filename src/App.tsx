78288cade3929eeb2eca682c5fbffc13ccab4e89
Jhonatan Sarmento <jhonatan.sarmento@gmail.com>
2023-08-11 11:37:30 -0400

    feat: set up initial project environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { AppThemeProvider } from './shared/context/';
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppThemeProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppThemeProvider>
   );
-}
-
-export default App;
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/context';
+
+export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
+  return (
+    <Routes>
+      <Route
+        path='/pagina-inicial'
+        element={
+          <Button variant='contained' color='primary' onClick={toggleTheme}>
+            Toggle Theme
+          </Button>
+        }
+      />
+      <Route path='*' element={<Navigate to='/pagina-inicial' />} />
+    </Routes>
+  );
+};
